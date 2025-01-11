0d9a1e12bcff57b2d3254d6b4b98d88b2f0875a7
yeyang <746659424@qq.com>
2025-01-12 01:45:47 +0800

    ✨ refactor(myKingHomepage): 重构段位背景和荣誉徽章显示逻辑
    - 移除对 libpag 的依赖，改用静态图片显示段位背景和荣誉徽章
    - 优化段位背景图片路径，使用动态插入方式
    - 根据玩家是否为王者段位，动态显示荣誉徽章或职业图标
    - 删除不必要的 canvas 元素和相关 JavaScript 代码

--- apps/myKingHomepage.js
+++ apps/myKingHomepage.js
@@ -184,18 +184,19 @@ export class MyKingHomepage extends plugin {
       winRateItem, // 胜率
       skinNumItem // 皮肤数量
     ] = mods
-    const { rankingStar, starImg, flagPag, honorPag } = JSON.parse(mode5v5.param1)
+    const { rankingStar, starImg } = JSON.parse(mode5v5.param1)
     const rank10v10 = `${mode10v10.name} ${JSON.parse(mode10v10.param1).rankingStar}星`
     const rank5v5 = `${mode5v5.name} ${rankingStar}星`
     const rankIcon = mode5v5.icon
     // 默认为4 王者后都不再处理
-    let flagImg = 'https://camp.qq.com/battle/profile/flagV2/4.png'
-    if (rank5v5.includes('青铜') || rank5v5.includes('白银') || rank5v5.includes('黄金') || rank5v5.includes('铂金')) flagImg = 'https://camp.qq.com/battle/profile/flagV2/1.png'
-    if (rank5v5.includes('钻石') || rank5v5.includes('星耀')) flagImg = 'https://camp.qq.com/battle/profile/flagV2/2.png'
-    if (rank5v5.includes('最强王者')) flagImg = 'https://camp.qq.com/battle/profile/flagV2/3.png'
+    let flagImg = '4'
+    if (rank5v5.includes('青铜') || rank5v5.includes('白银') || rank5v5.includes('黄金') || rank5v5.includes('铂金')) flagImg = '1'
+    if (rank5v5.includes('钻石') || rank5v5.includes('星耀')) flagImg = '2'
+    if (rank5v5.includes('最强王者')) flagImg = '3'
     console.log(mods)
     const isKing = rank5v5.includes('王者')
     const isOffline = gameOnline === '离线'
+    const honor = isKing? "honor" : "roleJob"
     const data = {
       tplFile: 'plugins/GloryOfKings-Plugin/resources/html/MyKingHomepage.html',
       _res_path: '../../../plugins/GloryOfKings-Plugin/resources/',
@@ -215,7 +216,7 @@ export class MyKingHomepage extends plugin {
       starImg,
       isKing,
       isOffline,
-      flagPag, honorPag,
+      honor,
       content_1: fightPowerItem.content,
       content_2: mvpNumItem.content,
       content_3: totalBattleCountItem.content,
