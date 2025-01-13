0702c2e548693d5bca4a89b08f002de0838f2ba7
叶殇 <197728340@qq.com>
2025-01-13 16:30:47 +0800

    ✨ feat(queryGameStats): 增加@管理员切换战绩推送状态功能
    - 添加对@管理员功能的支持，允许在群内通过@管理员来切换战绩推送状态
    - 优化了非群组消息的回复，增加了友好的错误提示

--- apps/queryGameStats.js
+++ apps/queryGameStats.js
@@ -143,10 +143,15 @@ export class QueryGameStats extends plugin {
 
   // 切换战绩推送状态的函数
   async toggleGameStatsPush (e) {
-    let userId = e.user_id // 获取用户 ID
+    let userId
+    if (e.at && e.isMaster) {
+      userId = e.at
+    } else {
+      userId = e.user_id
+    }
     let groupId = e.group_id // 获取群组 ID
     const { isGroup } = e // 检查是否为群组消息
-    if (!isGroup) return false // 如果不是群组消息，返回
+    if (!isGroup) return e.reply('只支持群内使用', true) // 如果不是群组消息，返回
 
     logger.debug(`用户 ${userId} 请求切换战绩推送状态...`)
 
